d615fa1cd13ae317ff1484f5016cd70c28500e20
root <nauyan@hotmail.com>
2020-05-30 17:49:06 +0500

    Final Day

---
+++ download.sh
@@ -0,0 +1,16 @@
+#wget https://zenodo.org/record/3723295/files/annotations.csv
+#wget https://zenodo.org/record/3723295/files/candidates.csv
+#wget https://zenodo.org/record/3723295/files/candidates_V2.zip
+#wget https://zenodo.org/record/3723295/files/evaluationScript.zip
+#wget https://zenodo.org/record/3723295/files/sampleSubmission.csv
+#wget https://zenodo.org/record/3723295/files/seg-lungs-LUNA16.zip
+wget https://zenodo.org/record/3723295/files/subset0.zip
+wget https://zenodo.org/record/3723295/files/subset1.zip
+wget https://zenodo.org/record/3723295/files/subset2.zip
+wget https://zenodo.org/record/3723295/files/subset3.zip
+wget https://zenodo.org/record/3723295/files/subset4.zip
+wget https://zenodo.org/record/3723295/files/subset5.zip
+wget https://zenodo.org/record/3723295/files/subset6.zip
+wget https://zenodo.org/record/3723299/files/subset7.zip
+wget https://zenodo.org/record/3723299/files/subset8.zip
+wget https://zenodo.org/record/3723299/files/subset9.zip
---
+++ extract.sh
@@ -0,0 +1,26 @@
+zip -FF subset1.zip -O subset1.fixed.zip
+unzip subset1.fixed.zip -d volumes/images/
+
+zip -FF subset2.zip -O subset2.fixed.zip
+unzip subset2.fixed.zip -d volumes/images/
+
+zip -FF subset3.zip -O subset3.fixed.zip
+unzip subset3.fixed.zip -d volumes/images/
+
+zip -FF subset4.zip -O subset4.fixed.zip
+unzip subset4.fixed.zip -d volumes/images/
+
+zip -FF subset5.zip -O subset5.fixed.zip
+unzip subset5.fixed.zip -d volumes/images/
+
+zip -FF subset6.zip -O subset6.fixed.zip
+unzip subset6.fixed.zip -d volumes/images/
+
+zip -FF subset7.zip -O subset7.fixed.zip
+unzip subset7.fixed.zip -d volumes/images/
+
+zip -FF subset8.zip -O subset8.fixed.zip
+unzip subset8.fixed.zip -d volumes/images/
+
+zip -FF subset9.zip -O subset9.fixed.zip
+unzip subset9.fixed.zip -d volumes/images/
